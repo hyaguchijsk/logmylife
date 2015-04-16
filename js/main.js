86f834576bd900dae3f9c20a387b6deb86ff6e2d
Hiroaki Yaguchi <h-yaguchi@jsk.t.u-tokyo.ac.jp>
2015-04-16 19:04:43 +0900

    add login action

--- js/main.js
+++ js/main.js
@@ -3,19 +3,22 @@ var client = new Dropbox.Client({key: "efvlr1x3fmx1l7v"});
 // Try to finish OAuth authorization.
 client.authenticate({interactive: false}, function (error) {
     if (error) {
-        alert('Authentication error: ' + error);
         $('#auth').text('Authentication error: ' + error);
     }
 });
 
-alert(client.isAuthenticated());
+// if can not login, try to login
 if (client.isAuthenticated()) {
-    $('#auth').text('auth...');
-    alert('auth...');
+    client.authenticate();
+}
+
+// authenticated?
+if (client.isAuthenticated()) {
     client.getAccountInfo(
         {},
         function (err, info, obj) {
-            alert('callback');
             $('#auth').text('Hello ' + info.name);
         });
+} else {
+    $('#auth').text('Authetication failed.');
 }
