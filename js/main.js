06892a4d235f53746adee0956139b24706899974
Hiroaki Yaguchi <h-yaguchi@jsk.t.u-tokyo.ac.jp>
2015-04-16 18:43:15 +0900

    add auth info

--- js/main.js
+++ js/main.js
@@ -3,10 +3,14 @@ var client = new Dropbox.Client({key: "efvlr1x3fmx1l7v"});
 // Try to finish OAuth authorization.
 client.authenticate({interactive: false}, function (error) {
     if (error) {
-        alert('Authentication error: ' + error);
+        $('#auth').text('Authentication error: ' + error);
     }
 });
 
 if (client.isAuthenticated()) {
-    // Client is authenticated. Display UI.
+    client.getAccountInfo(
+        {},
+        function (api, info, err) {
+            $('#auth').text('Hello ' + info.name);
+        });
 }
