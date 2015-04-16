23cd30796343b80d158e70ce853255c14964d95d
Hiroaki Yaguchi <h-yaguchi@jsk.t.u-tokyo.ac.jp>
2015-04-16 18:59:23 +0900

    add infomation

--- js/main.js
+++ js/main.js
@@ -3,14 +3,18 @@ var client = new Dropbox.Client({key: "efvlr1x3fmx1l7v"});
 // Try to finish OAuth authorization.
 client.authenticate({interactive: false}, function (error) {
     if (error) {
+        alert('Authentication error: ' + error);
         $('#auth').text('Authentication error: ' + error);
     }
 });
 
 if (client.isAuthenticated()) {
+    $('#auth').text('auth...');
+    alert('auth...');
     client.getAccountInfo(
         {},
-        function (api, info, err) {
+        function (err, info, obj) {
+            alert('callback');
             $('#auth').text('Hello ' + info.name);
         });
 }
